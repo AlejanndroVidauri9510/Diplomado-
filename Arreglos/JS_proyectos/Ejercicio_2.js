f8fd06efc2da13971d00bfb8b7d0df1948b8d7de
Alejandro <95542726+AlejanndroVidauri9510@users.noreply.github.com>
2024-09-21 01:59:33 -0600

    Add files via upload
    Actualizado

--- Arreglos/JS_proyectos/Ejercicio_2.js
+++ Arreglos/JS_proyectos/Ejercicio_2.js
@@ -2,9 +2,107 @@
 console.log('Hola mundo ');
 /* */
 alert("Estas es una alerta ");
-/* */
-prompt('¿Cual es tu edad ?')
+
+
 //document.write('Un Document.Write!');
+
+
+
+var firsName = 'Alejandro';
+
+let lastName = 'Vidauri ';
+/*Las constates no se reasignan las variables  */
+const insta = '@Manotas ' ;
+
+lastName = 'Contreras';
+console.log(firsName);
+console.log(lastName);
+console.log(insta);
+
+/*Aqui estoy creando un objeto */
+const yaxche={
+    nombre: 'Alejandro ',
+    Mascota : true,
+    insta: 'no tengo',
+    trabajo: 'Programador y futuro ganster chido ',
+};
+
+    console.log(yaxche);
+
+    let string = 'Este es un string de texto';
+    console.log(string);
+
+    let number = 5;
+    console.log(number);
+
+    let boolean = true;
+    console.log(boolean);
+
+    let boolean2 = false;
+    console.log(boolean2);
+
+    /*Operadores logicos nuevamente wey ya echale cabeza bro si puedes eres muy analitico se tactico  */
+
+    /*
+    Asignacion (=)
+
+    Suma (+)
+
+    Resta (-)
+
+    Multiplicación (*)
+
+    División (*)
+
+    Residuo de una división (%)
+
+    Exponencial (***)
+
+
+     */
+
+
+    let suma =5+27;
+    console.log(suma);
+    
+    let resta = 88-89;
+    console.log(resta);
+
+    let multiplicacion= 77*568;
+    console.log(multiplicacion);
+
+
+    let division = 5/8;
+    console.log(division);
+
+    let modulo= 80%7;
+    console.log(modulo);
+
+    let exponcial= 5 ** 2;
+    console.log(exponcial);
+
+    const a=5;
+
+    const b = 15;
+
+    console.log(a+b );
+    console.log(a-b);
+    
+    let edad2 = prompt('¿Que edad tienes?');
+    let  date = Number(edad2) ;
+
+    let tiempoFuturo =10;
+
+    console.log('En 10 años tendras ');
+
+    console.log(date  + tiempoFuturo + ' años ' );
+
+
+
+
+    
+
+
 
 
 
